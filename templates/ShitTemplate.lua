c3421bee4196b0b3a7617da98e94d30fffcf649b
Samson Gross <sag1053@thi.de>
2016-06-13 13:52:56 +0200

    added shit template

---
+++ templates/ShitTemplate.lua
@@ -0,0 +1,17 @@
+local loveframes = require("lib.Loveframes.libraries.templates");
+
+-- template table
+local template = {}
+
+-- template name
+template.name = "ShitTemplate"
+
+-- template properties
+template.properties = {}
+
+template.properties["imagebutton"] =
+{
+    hover = true
+}
+
+return template;
